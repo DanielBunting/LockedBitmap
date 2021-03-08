d37f6a6deebf0de371d4df4215eccca842636b12
Danie Bunting <daniel.bunting93@gmail.com>
2021-03-08 22:50:06 +0000

    Adds a new fluent interface for conversions.

---
+++ LockedBitmap/Extensions/FluentConversions.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace LockedBitmapUtil.Extensions
+{
+    public static class FluentConversions
+    {
+        public static LockedBitmap ToLockedBitmap(this Bitmap bitmap)
+        {
+            var newLockedBitmap = new LockedBitmap(bitmap);
+            newLockedBitmap.LockBits();
+
+            return newLockedBitmap;
+        }
+
+        public static Bitmap ToBitmap(this LockedBitmap lockedBitmap)
+        {
+            try
+            {
+                lockedBitmap.UnlockBits();
+                return lockedBitmap.Source;
+            }
+            finally
+            {
+                lockedBitmap.Dispose();
+            }
+        }
+    }
+}
